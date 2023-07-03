366e631875554872770ce48de3f3aa2b1998b656
Charles <charleszecevic@gmail.com>
2023-07-03 21:53:12 +0200

    add css

--- kasa-project/src/page/About.jsx
+++ kasa-project/src/page/About.jsx
@@ -24,87 +24,82 @@ export const About = () => {
 
   return (
     <>
-    <div className="imgAbout">
+      <div className="imgAbout">
         <img src={img} alt="aboutImg" />
-    </div>
-    <div className="allElementAbout">
-      <div className="oneElementAbout">
-        <button onClick={FirstElement}>
-          Fiabilité
-          {Fiabilité ? (
-            <FontAwesomeIcon icon={faArrowDown} />
-          ) : (
-            <FontAwesomeIcon icon={faArrowUp} />
+      </div>
+      <div className="allElementAbout">
+        <div className="oneElementAbout">
+          <button onClick={FirstElement}>
+            Fiabilité
+            {Fiabilité ? (
+              <FontAwesomeIcon icon={faArrowDown} />
+            ) : (
+              <FontAwesomeIcon icon={faArrowUp} />
+            )}
+          </button>
+          {Fiabilité && (
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
           )}
-        </button>
-        {Fiabilité ? (
-          <p>
-            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-            photos sont conformes aux logements, et toutes les informations sont
-            régulièrement vérifiées par nos équipes.
-          </p>
-        ) : (
-          !Fiabilité
-        )}
-      </div>
-      <div className="oneElementAbout">
-        <button onClick={SecondElement}>
-          Respect
-          {Respect ? (
-            <FontAwesomeIcon icon={faArrowDown} />
-          ) : (
-            <FontAwesomeIcon icon={faArrowUp} />
+        </div>
+        <div className="oneElementAbout">
+          <button onClick={SecondElement}>
+            Respect
+            {Respect ? (
+              <FontAwesomeIcon icon={faArrowDown} />
+            ) : (
+              <FontAwesomeIcon icon={faArrowUp} />
+            )}
+          </button>
+          {Respect && (
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.
+            </p>
           )}
-        </button>
-        {Respect ? (
-          <p>
-            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.
-          </p>
-        ) : (
-          !Respect
-        )}
-      </div>
-      <div className="oneElementAbout">
-        <button onClick={ThridElement}>Service
-        {Service ? (
-            <FontAwesomeIcon icon={faArrowDown} />
-          ) : (
-            <FontAwesomeIcon icon={faArrowUp} />
-          )}</button>
-        {Service ? (
-          <p>
-            Nos équipes se tiennent à votre disposition pour vous fournir une
-            expérience parfaite. N'hésitez pas à nous contacter si vous avez la
-            moindre question.
-          </p>
-        ) : (
-          !Service
-        )}
-      </div>
-      <div className="oneElementAbout">
-        <button onClick={FourthElement}>Sécurité
-        {Sécurité ? (
-            <FontAwesomeIcon icon={faArrowDown} />
-          ) : (
-            <FontAwesomeIcon icon={faArrowUp} />
+        </div>
+        <div className="oneElementAbout">
+          <button onClick={ThridElement}>
+            Service
+            {Service ? (
+              <FontAwesomeIcon icon={faArrowDown} />
+            ) : (
+              <FontAwesomeIcon icon={faArrowUp} />
+            )}
+          </button>
+          {Service && (
+            <p>
+              Nos équipes se tiennent à votre disposition pour vous fournir une
+              expérience parfaite. N'hésitez pas à nous contacter si vous avez
+              la moindre question.
+            </p>
           )}
-        </button>
-        {Sécurité ? (
-          <p>
-            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
-            pour les voyageurs, chaque logement correspond aux critères de
-            sécurité établis par nos services. En laissant une note aussi bien à
-            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
-            les standards sont bien respectés. Nous organisons également des
-            ateliers sur la sécurité domestique pour nos hôtes.
-          </p>
-        ) : (
-          !Sécurité
-        )}
+        </div>
+        <div className="oneElementAbout">
+          <button onClick={FourthElement}>
+            Sécurité
+            {Sécurité ? (
+              <FontAwesomeIcon icon={faArrowDown} />
+            ) : (
+              <FontAwesomeIcon icon={faArrowUp} />
+            )}
+          </button>
+          {Sécurité && (
+            <p>
+              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+              pour les voyageurs, chaque logement correspond aux critères de
+              sécurité établis par nos services. En laissant une note aussi bien
+              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+              que les standards sont bien respectés. Nous organisons également
+              des ateliers sur la sécurité domestique pour nos hôtes.
+            </p>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
