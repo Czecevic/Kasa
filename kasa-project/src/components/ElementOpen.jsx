4720f4854aee9c76b8d7350c73d49a5a3f20c1d9
Charles <charleszecevic@gmail.com>
2023-07-10 08:28:59 +0200

    push all elem

---
+++ kasa-project/src/components/ElementOpen.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import img from "../assets/chevron-up-solid.svg";
+
+export const ElementOpen = ({ className, title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={className}>
+      <button onClick={handleClick}>
+        {title}
+        <span className={`arrowImgOfElement ${isOpen ? "rotate-icon" : ""}`}>
+          <img src={img} alt="arrow-up" />
+        </span>
+      </button>
+      {isOpen && <p>{content}</p>}
+    </div>
+  );
+};
---
+++ kasa-project/src/page/Logement.jsx
@@ -0,0 +1,59 @@
+import { useParams } from "react-router-dom";
+import { callData } from "../callData";
+import { Error } from "./Error";
+import { StarRating } from "../components/StarRating";
+import { Carousel } from "../components/Carousel";
+import { ElementOpen } from "../components/ElementOpen";
+
+export const Logement = () => {
+  const data = callData();
+  const { id } = useParams();
+  // console.log(data);
+  const dataLocation = data.find((item) => item.id === id);
+  if (!dataLocation) {
+    return <Error />;
+  }
+
+  return (
+    <div className="allElementResa">
+      <div className="imgResa">
+        <Carousel images={dataLocation.pictures} />
+      </div>
+      <div className="profilAndTitre">
+        <div className="title">
+          <h2>{dataLocation.title}</h2>
+          <ul className="tag">
+            {dataLocation.tags.map((tagName, index) => (
+              <li key={index}>{tagName}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="user">
+          <div className="userDetail">
+            <h3>{dataLocation.host.name}</h3>
+            <img src={dataLocation.host.picture} alt="Host" />
+          </div>
+          <StarRating rating={dataLocation.rating} />
+        </div>
+      </div>
+      <div className="DescriptionAndEquipement">
+        <ElementOpen
+          className="Description"
+          title="Description"
+          content={dataLocation.description}
+        />
+        <ElementOpen
+          className="Equipement"
+          title="Equipement"
+          content={
+            <ul>
+              {dataLocation.equipments.map((equipementName, index) => (
+                <li key={index}>{equipementName}</li>
+              ))}
+            </ul>
+          }
+        />
+      </div>
+    </div>
+  );
+};
