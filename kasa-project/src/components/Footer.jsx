602d939cf882b4e7b76b9b3d52a7189de33598e3
Czecevic <czecevi@matrice.io>
2023-07-25 15:53:56 +0200

    modif file

--- kasa-project/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="footer">
-      <img src="./src/assets/logo.png" alt="logo" />
-      <p>&#169; 2020 Kasa. All rights reserved</p>
-    </footer>
-  );
-};
